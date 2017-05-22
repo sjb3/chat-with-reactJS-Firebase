1b6969e9c89e0a5a3e660fb062659a260e599c16
Sung J Byun <sungjbyun@D-173-250-206-55.dhcp4.washington.edu>
2017-05-21 20:48:21 -0700

    firebase is COOL

--- src/Components/ChatRoom.js
+++ src/Components/ChatRoom.js
@@ -2,20 +2,76 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 
 export class ChatRoom extends Component {
+
+  constructor(props, context) {
+    super(props, context)
+    this.updatedMessage = this.updatedMessage.bind(this);
+    this.submitMessage = this.submitMessage.bind(this);
+    this.state = {
+      message: '',
+      messages: [
+        // {id: 0, text: 'first msg'},
+        // {id: 1, text: 'second msg'},
+        // {id: 2, text: 'third msg'},
+        // {id: 3, text: 'fouth msg'}
+          ]
+        }
+      }
+
+  componentDidMount() {
+      console.log('componentDidMount');
+
+      firebase.database().ref('messages/').on('value', (snapshot) => {
+        const currentMessage = snapshot.val()
+        if (currentMessage != null){
+          this.setState({
+            messages: currentMessage
+          })
+        }
+      })
+  }
+
+  updatedMessage(e) {
+    console.log('updatedMessage: '+ e.target.value);
+    this.setState({
+      message: e.target.value
+    })
+  }
+
+  submitMessage(e) {
+    console.log('submitMessage'+ this.state.message);
+
+    const nextMessage = {
+      id: this.state.messages.length,
+      text: this.state.message
+    }
+    //send to firebase
+    firebase.database().ref('messages/'+nextMessage.id).set(nextMessage)
+
+    // var list = Object.assign([], this.state.messages);
+    // list.push(nextMessage)
+    // this.setState({
+    //   messages: list
+    // })
+  }
+
   render() {
+    const currentMessage = this.state.messages.map( (message, i) => {
+      return (
+        <li key = {message.id}>{message.text}</li>
+      )
+    })
+
     return (
       <div>
         <h1>This is the chat room</h1>
-        <ol>
-          <li>Message 1</li>
-          <li>Message 2</li>
-          <li>Message 3</li>
-          <li>Message 4</li>
+          <ol>
+            {currentMessage}
+          </ol>
 
-        </ol>
-        <input type="text" placeholder="Type here ..." />
+        <input onChange={this.updatedMessage} type="text" placeholder="Type here ..." />
         <br />
-        <button>Submit</button>
+        <button onClick={this.submitMessage}>Submit</button>
       </div>
     )
   }
